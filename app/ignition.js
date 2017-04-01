eaf06bac8febd6aed7cc81f9d7cd30853961ec0e
DaQuirm <leo.daquirm@gmail.com>
2017-04-01 19:37:23 +0200

    :hatching_chick:

---
+++ app/ignition.js
@@ -0,0 +1,58 @@
+const WebSocketClient = require('uws');
+const createPhoenix = require('phoenix');
+const { protocol: {frontService, stateService}, parseMessage, arnaux } = require('message-factory');
+
+const phoenix = createPhoenix(WebSocketClient, { uri: 'ws://localhost:3000', timeout: 500 });
+
+const puzzles = require('../data/puzzles.json');
+const users = require('../data/qd-masters.json');
+
+const puzzleIds = [];
+const participantIds = [];
+
+function createSessions () {
+    if (puzzleIds.length === puzzles.length && participantIds.length === users.length) {
+        participantIds.forEach(participantId => {
+            phoenix.send(stateService.sessionCreate(participantId, puzzles));
+        });
+    }
+}
+
+phoenix
+    .on('connected', () => {
+        console.log('[init-service]', 'phoenix is alive');
+        phoenix.send(arnaux.checkin('init-service'));
+        users.forEach(participant => {
+            phoenix.send(frontService.createParticipant(participant));
+        });
+        puzzles.forEach(puzzle => {
+            phoenix.send(stateService.puzzleCreate(puzzle));
+        });
+    })
+    .on('disconnected', () => {
+        console.error('[init-service]', 'phoenix disconnected');
+    })
+    .on('message', (incomingMessage) => {
+        const { message } = parseMessage(incomingMessage.data);
+
+        switch (message.name) {
+            case 'puzzle.created':
+                console.log('[init-service]', 'created puzzle', message.puzzleId);
+                puzzleIds.push(message.puzzleId);
+                createSessions();
+                break;
+
+            case 'participant.created':
+                console.log('[init-service]', 'created participant', message.participantId);
+                participantIds.push(message.participantId);
+                createSessions();
+                break;
+
+            case 'session.created':
+                console.log('[init-service]', 'created session', message.sessionId);
+                break;
+
+            default:
+                console.warn('[init-service]', 'unhandled message', message.name);
+        }
+    });
