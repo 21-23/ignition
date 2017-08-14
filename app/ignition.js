a7390205000959c807de2239e94fc6c3056111bb
Kiril Knysh <k.knysh@gmail.com>
2017-08-14 20:54:45 +0200

    Fix order in assets creation;

--- app/ignition.js
+++ app/ignition.js
@@ -10,30 +10,97 @@ const users = require('../data/qd-masters.json');
 const puzzleIds = [];
 const participantIds = [];
 
-const sessionAliasSuffixes = ['dq', 'kk', 'ms', 'av'];
+const sessionAliasSuffixes = ['dq', 'kk', 'ms', 'av']; // order here MUST be the same as in ../data/qd-masters.json
 
-function createSessions () {
-    if (puzzleIds.length === puzzles.length && participantIds.length === users.length) {
-        participantIds.forEach((participantId, index) => {
-            const alias = `_qd-dev-${sessionAliasSuffixes[index]}`;
-            phoenix.send(stateService.sessionCreate(participantId, alias, puzzleIds));
+function createSessions() {
+    participantIds.forEach((participantId, index) => {
+        const alias = `_qd-dev-${sessionAliasSuffixes[index]}`;
+        phoenix.send(stateService.sessionCreate(participantId, alias, puzzleIds));
+    });
+
+    phoenix.send(stateService.sessionCreate(participantIds[0], 'rs.krakow', puzzleIds.slice(1)));
+    phoenix.send(stateService.sessionCreate(participantIds[0], 'rs.krakow-demo', [puzzleIds[0]]));
+}
+
+function createPuzzles(puzzles) {
+    return new Promise((resolve) => {
+        function onPuzzleCreated(incomingMessage) {
+            const { message } = parseMessage(incomingMessage.data);
+
+            if (message.name !== 'puzzle.created') {
+                return;
+            }
+
+            console.log('[init-service]', 'created puzzle', message.puzzleId);
+            puzzleIds.push(message.puzzleId);
+
+            if (puzzleIds.length === puzzles.length) {
+                phoenix.off('message', onPuzzleCreated);
+                resolve();
+            }
+        }
+
+        phoenix.on('message', onPuzzleCreated);
+
+        puzzles.forEach(puzzle => {
+            phoenix.send(stateService.puzzleCreate(puzzle));
         });
+    });
+}
 
-        phoenix.send(stateService.sessionCreate(participantIds[0], 'rs.krakow', puzzleIds.slice(1)));
-        phoenix.send(stateService.sessionCreate(participantIds[0], 'rs.krakow-demo', [puzzleIds[0]]));
-    }
+function createParticipant(user) {
+    return new Promise((resolve) => {
+        function onParticipantCreated(incomingMessage) {
+            const { message } = parseMessage(incomingMessage.data);
+
+            if (message.name !== 'participant.created') {
+                return;
+            }
+
+            console.log('[init-service]', 'created participant', message.participantId);
+            participantIds.push(message.participantId);
+            phoenix.off('message', onParticipantCreated);
+            resolve();
+        }
+
+        phoenix.on('message', onParticipantCreated);
+
+        phoenix.send(frontService.createParticipant(user));
+    });
+}
+
+function createParticipantsInOrder(users) {
+    return new Promise((resolve) => {
+        return createParticipant(users.shift())
+            .then(() => {
+                if (!users.length) {
+                    return resolve();
+                }
+
+                return createParticipantsInOrder(users)
+                    .then(resolve);
+            });
+    });
+}
+
+function createParticipants(users) {
+    const usersCopy = Array.from(users);
+
+    return createParticipantsInOrder(usersCopy);
 }
 
 phoenix
     .on('connected', () => {
         console.log('[init-service]', 'phoenix is alive');
         phoenix.send(arnaux.checkin('init-service'));
-        users.forEach(participant => {
-            phoenix.send(frontService.createParticipant(participant));
-        });
-        puzzles.forEach(puzzle => {
-            phoenix.send(stateService.puzzleCreate(puzzle));
-        });
+
+        createPuzzles(puzzles)
+            .then(() => {
+                return createParticipants(users);
+            })
+            .then(() => {
+                return createSessions();
+            });
     })
     .on('disconnected', () => {
         console.error('[init-service]', 'phoenix disconnected');
@@ -42,23 +109,11 @@ phoenix
         const { message } = parseMessage(incomingMessage.data);
 
         switch (message.name) {
-            case 'puzzle.created':
-                console.log('[init-service]', 'created puzzle', message.puzzleId);
-                puzzleIds.push(message.puzzleId);
-                createSessions();
-                break;
-
-            case 'participant.created':
-                console.log('[init-service]', 'created participant', message.participantId);
-                participantIds.push(message.participantId);
-                createSessions();
-                break;
-
             case 'session.created':
                 console.log('[init-service]', 'created session', message.sessionId);
                 break;
 
             default:
-                console.warn('[init-service]', 'unhandled message', message.name);
+                console.log('[init-service]', 'unhandled message', message.name);
         }
     });
